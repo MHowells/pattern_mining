1453fb8fc9b129dae5c84618c9a86882ed15a479
Matthew Howells <howellsmatthew@hotmail.co.uk>
2024-10-29 15:31:49 +0000

    Update recursive function and merge tests
    Update an error from the last commit. Merge the
    tests for the now merged recursive functions from
    the previous commit.

--- tests.py
+++ tests.py
@@ -733,6 +733,53 @@ def test_check_is_deterministic_with_multiple_pairs():
     assert obtained_nondeterministic_pairs == expected_nondeterministic_pairs
 
 
+arnolds_pathway_matrix_after_merges = np.array(
+    [
+        [
+            [0, 10, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 8, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2],
+            [0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        ],
+        [
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 4, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 4, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        ],
+        [
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 2, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 2, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        ],
+    ]
+)
+arnolds_states_after_merges = ["*", 0, 1, 3, 4, 5, 6, 7, 8, 9, 12]
+arnolds_red_states_after_merges = [0, 1, 12]
+
+
 def test_recursive_merge_two_states():
     # Test where states are merged recursively
     (
@@ -841,77 +888,21 @@ def test_recursive_merge_two_states():
     assert obtained_states == arnolds_states
     assert obtained_recursive_merge == False
 
-
-def test_get_blue_states():
-    obtained_blue_states = pattern_mining.get_blue_states(
-        pathway_matrix, example_red_states, states
-    )
-    expected_blue_states = [1, 2]
-    assert obtained_blue_states == expected_blue_states
-
-    obtained_blue_states = pattern_mining.get_blue_states(
-        arnolds_pathway_matrix, example_red_states, arnolds_states
-    )
-    expected_blue_states = [1, 2]
-    assert obtained_blue_states == expected_blue_states
-
-
-arnolds_pathway_matrix_after_merges = np.array(
-    [
-        [
-            [0, 10, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-            [0, 0, 8, 0, 0, 0, 0, 0, 0, 0, 0],
-            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2],
-            [0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0],
-            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-            [0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0],
-            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-        ],
-        [
-            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-            [0, 4, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-            [0, 0, 0, 4, 0, 0, 0, 0, 0, 0, 0],
-            [0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0],
-            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-        ],
-        [
-            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-            [0, 0, 2, 0, 0, 0, 0, 0, 0, 0, 0],
-            [0, 0, 0, 0, 2, 0, 0, 0, 0, 0, 0],
-            [0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0],
-            [0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0],
-            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-        ],
-    ]
-)
-arnolds_states_after_merges = ["*", 0, 1, 3, 4, 5, 6, 7, 8, 9, 12]
-arnolds_red_states_after_merges = [0, 1, 12]
-
-
-def test_recursive_merge_two_states_higuera():
-    # Test where states are merged recursively
+    # Test where states are merged recursively (Higuera)
     (
         obtained_matrix,
         obtained_states,
         obtained_recursive_merge,
         red_states,
-    ) = pattern_mining.recursive_merge_two_states_higuera(
-        1, 2, pathway_matrix, states, 0.2, alphabet, example_red_states
+    ) = pattern_mining.recursive_merge_two_states(
+        1,
+        2,
+        pathway_matrix,
+        states,
+        0.2,
+        alphabet,
+        example_red_states,
+        method="Higuera",
     )
     expected_matrix = np.array(
         [
@@ -953,8 +944,15 @@ def test_recursive_merge_two_states_higuera():
         obtained_states,
         obtained_recursive_merge,
         red_states,
-    ) = pattern_mining.recursive_merge_two_states_higuera(
-        0, 1, arnolds_pathway_matrix, arnolds_states, 0.2, alphabet, example_red_states
+    ) = pattern_mining.recursive_merge_two_states(
+        0,
+        1,
+        arnolds_pathway_matrix,
+        arnolds_states,
+        0.2,
+        alphabet,
+        example_red_states,
+        method="Higuera",
     )
     expected_matrix = np.array(
         [
@@ -1003,13 +1001,13 @@ def test_recursive_merge_two_states_higuera():
     assert obtained_recursive_merge == expected_recursive_merge
     assert red_states == example_red_states
 
-    # Test where Hoeffding's Bound fails during recursive merge
+    # Test where Hoeffding's Bound fails during recursive merge (Higuera)
     (
         obtained_matrix,
         obtained_states,
         obtained_recursive_merge,
         red_states,
-    ) = pattern_mining.recursive_merge_two_states_higuera(
+    ) = pattern_mining.recursive_merge_two_states(
         0,
         3,
         arnolds_pathway_matrix,
@@ -1017,13 +1015,14 @@ def test_recursive_merge_two_states_higuera():
         0.9,
         arnolds_alphabet,
         example_red_states,
+        method="Higuera",
     )
     assert np.allclose(obtained_matrix, arnolds_pathway_matrix)
     assert obtained_states == arnolds_states
     assert obtained_recursive_merge == False
     assert red_states == example_red_states
 
-    # Test where a red state gets merged during the recursive merge
+    # Test where a red state gets merged during the recursive merge (Higuera)
     expected_matrix = np.array(
         [
             [
@@ -1063,7 +1062,7 @@ def test_recursive_merge_two_states_higuera():
         obtained_states,
         obtained_recursive_merge,
         red_states,
-    ) = pattern_mining.recursive_merge_two_states_higuera(
+    ) = pattern_mining.recursive_merge_two_states(
         1,
         3,
         arnolds_pathway_matrix_after_merges,
@@ -1071,11 +1070,26 @@ def test_recursive_merge_two_states_higuera():
         0.9,
         arnolds_alphabet,
         arnolds_red_states_after_merges,
+        method="Higuera",
     )
     assert np.allclose(obtained_matrix, expected_matrix)
     assert obtained_states == expected_states
     assert obtained_recursive_merge == expected_recursive_merge
     assert red_states == expected_red_states
+
+
+def test_get_blue_states():
+    obtained_blue_states = pattern_mining.get_blue_states(
+        pathway_matrix, example_red_states, states
+    )
+    expected_blue_states = [1, 2]
+    assert obtained_blue_states == expected_blue_states
+
+    obtained_blue_states = pattern_mining.get_blue_states(
+        arnolds_pathway_matrix, example_red_states, arnolds_states
+    )
+    expected_blue_states = [1, 2]
+    assert obtained_blue_states == expected_blue_states
 
 
 def test_get_pairs_to_check():
